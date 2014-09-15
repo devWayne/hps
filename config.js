9c3de7cc8ceff831b52cf1336e24b1fb0ecf2b0f
Wayne <dev.wayne.ix@gmail.com>
2014-09-15 10:35:42 +0800

    chgname2hps

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ var port=8000;
 exports.map=[
 	['http://www.baidu.com','/Users/wayne/code/dianping/qqzone-fd-cdn/index.html'],
 	['http://www.taobao1.com','/Users/wayne/code/dianping/qqzone-fd-cdn/index.html'],
+	['http://s1.51ping.com/b/js/app/tuangou/deliverview.min.4b16bac2445d329978a26e608e324aa3.js','/Users/wayne/code/dianping/bc-tuangou-static/js/deliverview.js']
 ];
 exports.port=port;
